91c75ba972907dc88fa5ebc1804e2f0b7c5d1040
AngryCodes0404 <angrybird.codes@gmail.com>
2025-11-17 08:27:38 -0500

    create dev branch

--- setup.py
+++ setup.py
@@ -10,25 +10,25 @@ if not torch.cuda.is_available():
 else:
     ext_modules = [
         CUDAExtension(
-            name='fastmqa_cuda',
-            sources=['kernels/mqa_extension.cpp', 'kernels/mqa_kernel.cu'],
+            name="fastmqa_cuda",
+            sources=["kernels/mqa_extension.cpp", "kernels/mqa_kernel.cu"],
             extra_compile_args={
-                'cxx': ['-O3'],
-                'nvcc': [
-                    '-O3',
-                    '--use_fast_math',
-                    '-gencode=arch=compute_70,code=sm_70',
-                    '-gencode=arch=compute_75,code=sm_75',
-                    '-gencode=arch=compute_80,code=sm_80',
-                    '-gencode=arch=compute_86,code=sm_86',
-                ]
-            }
+                "cxx": ["-O3"],
+                "nvcc": [
+                    "-O3",
+                    "--use_fast_math",
+                    "-gencode=arch=compute_70,code=sm_70",
+                    "-gencode=arch=compute_75,code=sm_75",
+                    "-gencode=arch=compute_80,code=sm_80",
+                    "-gencode=arch=compute_86,code=sm_86",
+                ],
+            },
         )
     ]
 
 setup(
-    name='fastmqa',
+    name="fastmqa",
     ext_modules=ext_modules,
-    cmdclass={'build_ext': BuildExtension},
-    install_requires=['torch>=2.0.0'],
+    cmdclass={"build_ext": BuildExtension},
+    install_requires=["torch>=2.0.0"],
 )
